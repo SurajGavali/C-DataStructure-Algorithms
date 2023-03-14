99764a1eb8228fa59de0fe79466b737c8f97e500
SurajGavali <65647939+SurajGavali@users.noreply.github.com>
2023-03-14 17:01:29 +0000

    added deleting any node function

--- Linked-List/Add-Element-At-Any-Position.c
+++ Linked-List/Add-Element-At-Any-Position.c
@@ -10,7 +10,33 @@ struct Node {
 
 void InsertElementAtAnyPos(int p,int v);
 void Print();
+void Delete();
 struct Node* head;
+
+void Delete(int n){
+
+    struct Node* temp1 = head;
+    int i;
+
+    if(n==1){
+
+        head = temp1->next;
+        free(temp1);
+    }
+    else{
+
+        for(i=0;i<n-2;i++){
+
+            temp1 = temp1->next;
+        }
+
+        struct Node* temp2 = temp1->next;
+
+        temp1->next = temp2->next;
+        free(temp2);
+    }
+
+}
 void Print(){
 
 	struct Node* temp3 = head; //created a temporary pointer to traverse the list and assigned to head value so that head remain unchanged
@@ -52,24 +78,43 @@ void InsertElementAtAnyPos(int p,int v){
 int main(){
 
     head = NULL;
+    int del;
 
-    int pos,num,flag=1;
+    int pos,num,flag=1,count=1,delpos;
     
     while(flag != 0){
 
-        printf("Kindly Enter whole number between 0 and %d",count);
-        printf("Enter a position :: ");
+        printf("\nKindly Enter whole number between 0 and %d",count);
+        printf("\nEnter a position to add number :: ");
         scanf("%d",&pos);
-        printf("Enter a Number you want to add :: ");
+        printf("\nEnter a Number you want to add :: ");
         scanf("%d",&num);
         printf("\n");
 
         InsertElementAtAnyPos(pos,num);
+	count++;
         Print();
-        printf("You want to exit? if yes enter 0 or else enter 1 :: ");
+
+        printf("Do you want to delete any number from list?(1/0) :: ");
+	scanf("%d",&del);
+
+	printf("\nvalue of del :: %d",del);
+        if(del){
+            printf("\nEnter Position to delete number :: ");
+            scanf("%d",&delpos);
+
+            if(delpos<count){
+                Delete(delpos);
+		        Print();
+		        count--;
+            }
+            else{
+                printf("\nPlease enter valid position!!!");
+            }
+            
+	}
+	printf("\nYou want to exit? if yes enter 0 or else enter 1 :: ");
         scanf("%d",&flag);
     }
-
-
     return 0;
 }
